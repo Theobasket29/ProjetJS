1bfd84b280f3bef104ecad2471c926d2c439b0f7
Theobasket29 <blancotheo29@gmail.com>
2024-11-18 10:33:46 +0100

    Update donne.js
    Rajout des requettes de récupération des données

---
+++ donne.js
@@ -0,0 +1,9 @@
+let requestNA = $.ajax({
+    type:"GET",
+    url: "../Data/survey_results_NA.json",
+})
+
+let requestWE = $.ajax({
+    type:"GET",
+    url: "../Data/survey_results_WE.json",
+})
